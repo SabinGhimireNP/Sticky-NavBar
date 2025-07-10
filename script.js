0a85c950a2effbcf58c8243de7f7a32b8c8f97a0
Sabin Ghimire <sabinghimire223@gmail.com>
2025-07-10 21:24:58 +0545

    Console.log removed:

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const nav = document.querySelector("nav");
 const navTop = nav.offsetTop;
+
 function fixNav() {
   //   console.log(navTop);
   if (window.scrollY >= navTop) {
     document.body.style.paddingTop = `${nav.offsetHeight}px`;
-    console.log("padding added");
+    // console.log("padding added");
     document.body.classList.add("fixed-nav");
   } else {
     document.body.style.paddingTop = 0;
-    console.log("padding removed");
+    // console.log("padding removed");
 
     document.body.classList.remove("fixed-nav");
   }
